6cc92fc5029d9d10c319c8ece14e93cca979b128
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-11 01:20:52 -0300

    Displaying data from state, inside Resumo component.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import MenuSuperior from './components/MenuSuperior'
+import Resumo from './components/Resumo'
 
 function App() {
   return (
     <div>
       <MenuSuperior />
+      <Resumo />
     </div>
   );
 }
--- src/components/Resumo/index.js
+++ src/components/Resumo/index.js
@@ -4,8 +4,8 @@ export default class Resumo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            consultas : {
-                consultas_30dias_anteriores : 0,
+            consultas: {
+                consultas_30dias_anteriores: 0,
                 consultas_30dias_posteriores: 0
             },
             faturamento: {
@@ -24,7 +24,65 @@ export default class Resumo extends Component {
     render() {
         return (
             <div>
-
+                <h2 className="mt-2">Resumo</h2>
+                <div className="row">
+                    <div className="col">
+                        <h3>Consultas</h3>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div className="card-body">
+                                        {this.state.consultas.consultas_30dias_anteriores}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">
+                                        Próximos 30 dias
+                                    </div>
+                                    <div className="card-body">
+                                        {this.state.consultas.consultas_30dias_posteriores}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <h3>Faturamento</h3>
+                        <div className="row">
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">
+                                        30 dias anteriores
+                                    </div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.anterior.valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                                        <span className="badge badge-success ml-1">
+                                            {this.state.faturamento.anterior.comparativo} %
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card mt-2 text-center">
+                                    <div className="card-header">
+                                        Próximos 30 dias
+                                    </div>
+                                    <div className="card-body">
+                                        {this.state.faturamento.previsao.valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                                        <span className="badge badge-success ml-1">
+                                            {this.state.faturamento.previsao.comparativo} %
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
