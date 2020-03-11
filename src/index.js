fea3f52b83b1e38cbc14eec7e47b5e38fd1050b4
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-11 02:23:05 -0300

    the react-router-dom library has been installed and imported ad src/index.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './include/bootstrap'
-ReactDOM.render(<App />, document.getElementById('root'));
+import './include/bootstrap';
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
